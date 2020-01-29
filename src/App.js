f953ef901ba9d7d7897c18860d8f348b2f923ba7
rinki-ku <shrgpt044@gmail.com>
2020-01-29 10:52:03 +0100

    updated files

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Revolut Currency Exchange</h1>
+        <h1>Currency Exchange App</h1>
         <div className="app-wrapper">
           <ConnectedLiveRating />
           <Fragment>
